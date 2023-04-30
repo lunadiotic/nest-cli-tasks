df610be118836339adb80904f60ed0584c483bfb
aimeliala <idstack@outlook.co.id>
2023-04-30 23:49:34 +0700

    define route on controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get()
+  listTasks() {
+    return 'This action returns all tasks';
+  }
+
+  @Post()
+  createTask() {
+    return 'This action creates a new task';
+  }
+
+  @Get('/:id')
+  getTask() {
+    return 'This action returns a #task';
+  }
+}
