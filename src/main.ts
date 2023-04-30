28eabf43da4e4e05879e0712cd34d3286fa3a9a0
aimeliala <idstack@outlook.co.id>
2023-05-01 00:38:09 +0700

    setup global pipe for validationpipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TasksModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
