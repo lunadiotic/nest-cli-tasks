d55b4895e4f7ef1837b60fa15e4d40d3872885d4
aimeliala <idstack@outlook.co.id>
2023-05-01 16:13:52 +0700

    create a repository for tasks module

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { readFile, writeFile } from 'fs/promises';
+
+export class TasksRepository {
+  async findAll(): Promise<any> {
+    const data = await readFile('tasks.json', 'utf-8');
+    return JSON.parse(data);
+  }
+
+  async findOne(id: number) {
+    const data = await readFile('tasks.json', 'utf-8');
+    const tasks = JSON.parse(data);
+
+    return tasks.find((task: any) => task.id === id);
+  }
+
+  async create(task: string) {
+    const data = await readFile('tasks.json', 'utf-8');
+    const tasks = JSON.parse(data);
+
+    const newTask = {
+      id: tasks[tasks.length - 1].id + 1,
+      content: task,
+    };
+
+    tasks.push(newTask);
+
+    await writeFile('tasks.json', JSON.stringify(tasks, null, 2), 'utf-8');
+  }
+}
