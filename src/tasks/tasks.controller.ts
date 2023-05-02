64bcff86259877972292095573d781b6cf662396
aimeliala <idstack@outlook.co.id>
2023-05-02 13:21:29 +0700

    show error with exceptions

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { TasksService } from './tasks.service';
 
@@ -21,7 +28,11 @@ export class TasksController {
   }
 
   @Get('/:id')
-  getTask(@Param('id') id: string) {
-    return this.taskService.findOne(parseInt(id));
+  async getTask(@Param('id') id: string) {
+    const task = await this.taskService.findOne(parseInt(id));
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
   }
 }
